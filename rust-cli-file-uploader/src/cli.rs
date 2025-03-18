b8cb57e51aa6195914c927369db3aa015c854349
anonym-266 <desmondtardzenyuy@gmail.com>
2025-03-18 21:29:01 +0100

    added more description to the struct for the arguments

--- rust-cli-file-uploader/src/cli.rs
+++ rust-cli-file-uploader/src/cli.rs
@@ -3,7 +3,8 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
+#[structopt(name = "rust-cli-file-uploader", about = "Use an HTTP client (such as reqwest) to send the file to the upload endpoint on the server in this same workspace")]
 pub struct CliUploader {
-  #[structopt(short, long, parse(from_os_str))]
+  #[structopt(short = "-f", long, parse(from_os_str))]
   pub file: PathBuf
 }
