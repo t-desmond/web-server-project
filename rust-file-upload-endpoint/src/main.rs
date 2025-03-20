b2d8318e9a8b8d813fd89d3f0c9a95d692b6435c
anonym-266 <desmondtardzenyuy@gmail.com>
2025-03-20 17:27:02 +0100

    added instruction to create destination directory if it doesn't exist on the server

--- rust-file-upload-endpoint/src/main.rs
+++ rust-file-upload-endpoint/src/main.rs
@@ -8,7 +8,7 @@ use tokio::net::TcpListener;
 async fn main() {
     let app = Router::new().route("/upload", get(index).post(upload));
 
-    let listener = TcpListener::bind("localhost:5050/upload").await.unwrap();
+    let listener = TcpListener::bind("localhost:5050").await.unwrap();
 
     let address = listener.local_addr().unwrap();
 
--- rust-file-upload-endpoint/src/routes.rs
+++ rust-file-upload-endpoint/src/routes.rs
@@ -1,46 +1,54 @@
 use axum::{extract::Multipart, response::Html};
-use std::{fs::File, io::Write};
+use std::{
+    fs::{self, File},
+    io::Write,
+};
 use tera::{Context, Tera};
 
 pub async fn index() -> Html<String> {
-  let tera = match Tera::new("templates/**/*.html") {
-      Ok(t) => t,
-      Err(e) => {
-          eprintln!("Error initializing Tera: {}", e);
-          return Html("Error loading templates".to_string());
-      }
-  };
+    let tera = match Tera::new("templates/**/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Error initializing Tera: {}", e);
+            return Html("Error loading templates".to_string());
+        }
+    };
 
-  let context = Context::new();
-  match tera.render("index.html", &context) {
-      Ok(rendered) => Html(rendered),
-      Err(e) => {
-          eprintln!("Error rendering template: {}", e);
-          Html("Error rendering template".to_string())
-      }
-  }
+    let context = Context::new();
+    match tera.render("index.html", &context) {
+        Ok(rendered) => Html(rendered),
+        Err(e) => {
+            eprintln!("Error rendering template: {}", e);
+            Html("Error rendering template".to_string())
+        }
+    }
 }
 
 pub async fn upload(mut multipart: Multipart) {
-  while let Some(field) = multipart
-      .next_field()
-      .await
-      .expect("failed to extract field")
-  {
-      if field.name().unwrap() != "fileupload" {
-          continue;
-      }
-      
-      let file_name = field.file_name().unwrap();
-      
-      println!("Got file {}", file_name);
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .expect("failed to extract field")
+    {
+        if field.name().unwrap() != "fileupload" {
+            continue;
+        }
 
-      let file_path = format!("files/{}", file_name);
+        let file_name = field.file_name().unwrap();
 
-      let data = field.bytes().await.unwrap();
+        println!("Got file {}", file_name);
 
-      let mut file_handle = File::create(file_path).expect("failed to open file handle");
+        if fs::create_dir_all("files").is_ok() {
+        } else {
+            println!("Failed to create destination directory");
+        }
 
-      file_handle.write_all(&data).expect("failed to write data");
-  }
+        let file_path = format!("files/{}", file_name);
+
+        let data = field.bytes().await.unwrap();
+
+        let mut file_handle = File::create(file_path).expect("failed to open file handle");
+
+        file_handle.write_all(&data).expect("failed to write data");
+    }
 }
