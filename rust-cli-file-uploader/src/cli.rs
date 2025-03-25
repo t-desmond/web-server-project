aa356cb03afc691678ff994113564bc3f217f929
anonym-266 <desmondtardzenyuy@gmail.com>
2025-03-25 17:39:06 +0100

    updated cli to upload compressed files

--- rust-cli-file-uploader/src/cli.rs
+++ rust-cli-file-uploader/src/cli.rs
@@ -1,8 +1,9 @@
 use reqwest::{multipart, Client, Url};
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
+use rust_cli_file_uploader::compress;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -23,24 +24,24 @@ impl CliUploader {
         let url = Url::parse(self.url.as_str())?;
 
         for file in &self.files {
-            let file_content = fs::read(&file)?;
-            let c = file.clone();
-            let path = c.display().to_string();
+            let compressed_data = compress(&file)?;
+
             let resource_name = file
-                .file_name()
-                .unwrap_or_default()
-                .to_string_lossy()
-                .to_string();
-            println!("Resource Name: {}, {}", resource_name, path);
-    
-            let part = multipart::Part::bytes(file_content).file_name(resource_name.clone());
-    
+            .file_name()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string();
+
+            println!("Resource Name: {}", resource_name);
+
+            let part = multipart::Part::bytes(compressed_data).file_name(resource_name.clone());
+
             let form = reqwest::multipart::Form::new()
                 .text("resourceName", resource_name)
                 .part("fileupload", part);
-    
+
             let response = client.post(url.clone()).multipart(form).send().await?;
-    
+
             if response.status().is_success() {
                 println!("file uploaded.. status: {}", response.status())
             } else {
@@ -54,28 +55,29 @@ impl CliUploader {
     #[allow(dead_code)]
     pub async fn upload_file_1(&self) -> Result<(), Box<dyn std::error::Error>> {
         for file_ in &self.files {
-
             let mut file = File::open(&file_)?;
-    
+
             let mut file_data = Vec::new();
-    
+
             file.read_to_end(&mut file_data)?;
-    
+
             let file_name = file_.file_name().unwrap().to_string_lossy().to_string();
-    
+
             let form = multipart::Form::new().part(
                 "fileupload",
                 multipart::Part::bytes(file_data).file_name(file_name),
             );
-    
+
             let response = Client::new()
                 .post(self.url.as_str())
                 .multipart(form)
                 .send()
                 .await?;
-    
+
             if response.status().is_success() {
-                println!("file sent succesfully")
+                println!("file uploaded.. status: {}", response.status())
+            } else {
+                println!("failed to upload file. \n{}", response.status())
             }
         }
 
